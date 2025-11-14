30ccdaed47fa381942a0fd051d2cd0fe1f1c9961
Louis Croci <louis.croci@icloud.com>
2025-11-14 21:32:54 +0100

    feat: :sparkles: adding LGVL lib conf

---
+++ include/lv_conf.h
@@ -0,0 +1,88 @@
+/**
+ * @file lv_conf.h
+ * Configuration for LVGL v8.3
+ */
+
+#ifndef LV_CONF_H
+#define LV_CONF_H
+
+/* Color settings */
+#define LV_COLOR_DEPTH 16
+#define LV_COLOR_16_SWAP 0
+
+/* Memory settings */
+#define LV_MEM_CUSTOM 0
+#define LV_MEM_SIZE (48 * 1024U)
+
+/* Display settings */
+#define LV_HOR_RES_MAX 480
+#define LV_VER_RES_MAX 320
+#define LV_DPI_DEF 130
+
+/* Input device settings */
+#define LV_INDEV_DEF_READ_PERIOD 30
+#define LV_INDEV_DEF_DRAG_LIMIT 10
+#define LV_INDEV_DEF_DRAG_THROW 10
+#define LV_INDEV_DEF_LONG_PRESS_TIME 400
+#define LV_INDEV_DEF_LONG_PRESS_REP_TIME 100
+
+/* Feature usage */
+#define LV_USE_ANIMATION 1
+#define LV_USE_SHADOW 1
+#define LV_USE_BLEND_MODES 1
+#define LV_USE_OPA_SCALE 1
+#define LV_USE_IMG_TRANSFORM 1
+#define LV_USE_GROUP 1
+#define LV_USE_GPU 0
+#define LV_USE_FILESYSTEM 0
+#define LV_USE_USER_DATA 1
+
+/* Font settings */
+#define LV_FONT_MONTSERRAT_12 1
+#define LV_FONT_MONTSERRAT_14 1
+#define LV_FONT_MONTSERRAT_16 1
+#define LV_FONT_MONTSERRAT_18 1
+#define LV_FONT_MONTSERRAT_20 1
+#define LV_FONT_MONTSERRAT_22 1
+#define LV_FONT_MONTSERRAT_24 1
+#define LV_FONT_MONTSERRAT_26 1
+#define LV_FONT_MONTSERRAT_28 1
+
+#define LV_FONT_DEFAULT &lv_font_montserrat_16
+
+/* Theme */
+#define LV_USE_THEME_DEFAULT 1
+#define LV_THEME_DEFAULT_DARK 0
+
+/* Text settings */
+#define LV_TXT_ENC LV_TXT_ENC_UTF8
+#define LV_TXT_BREAK_CHARS " ,.;:-_"
+#define LV_TXT_LINE_BREAK_LONG_LEN 0
+#define LV_TXT_COLOR_CMD "#"
+
+/* Widget usage */
+#define LV_USE_ARC 1
+#define LV_USE_BAR 1
+#define LV_USE_BTN 1
+#define LV_USE_BTNMATRIX 1
+#define LV_USE_CANVAS 1
+#define LV_USE_CHECKBOX 1
+#define LV_USE_DROPDOWN 1
+#define LV_USE_IMG 1
+#define LV_USE_LABEL 1
+#define LV_USE_LINE 1
+#define LV_USE_ROLLER 1
+#define LV_USE_SLIDER 1
+#define LV_USE_SWITCH 1
+#define LV_USE_TEXTAREA 1
+#define LV_USE_TABLE 1
+
+/* Layouts */
+#define LV_USE_FLEX 1
+#define LV_USE_GRID 1
+
+/* Others */
+#define LV_USE_LOG 0
+#define LV_SPRINTF_CUSTOM 0
+
+#endif /* LV_CONF_H */
